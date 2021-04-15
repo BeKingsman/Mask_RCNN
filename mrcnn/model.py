fd06294c1cd44068486f3d025325dca82afe64b8
BeKingsman <53928332+BeKingsman@users.noreply.github.com>
2021-04-16 00:20:06 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1829,9 +1829,9 @@ class MaskRCNN():
         config: A Sub-class of the Config class
         model_dir: Directory to save training logs and trained weights
         """
-	print("Harsh\n\n\n\n")
 	l_f= open("/content/Mask_RCNN/log_file.txt","w")
 	l_f.write("Init Called")
+	print("Harsh\n\n\n\n")
 	l_f.close() 
 	      
         assert mode in ['training', 'inference']
