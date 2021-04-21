4313bb73dd2030c4014d09f9c64ed5070abb612a
HARSH AGARWAL <harshagarwal8055@gmail.com>
2021-04-21 23:21:04 +0530

    debug

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -63,18 +63,18 @@ def get_ax(rows=1, cols=1, size=16):
     _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
     return ax
 
-def custom_filtering(img,proposals):
-  ans=[]
-  print("\nPrinting Img:\n")
-  print(img)
-#   print(img.shape)
-  print("\n\nPrinting Proposals:\n")
-  print(proposals)
-  for i in tf.range(proposals.shape[0]):
-      print(proposals[i])
-#   print(proposals.shape)
+# def custom_filtering(img,proposals):
+#   ans=[]
+#   print("\nPrinting Img:\n")
+#   print(img)
+# #   print(img.shape)
+#   print("\n\nPrinting Proposals:\n")
+#   print(proposals)
+#   for i in tf.range(proposals.shape[0]):
+#       print(proposals[i])
+# #   print(proposals.shape)
 
-  return proposals
+#   return proposals
 #   for i in range(proposals.shape[0]):
 #     # print(proposals[i])
 #     ly=proposals[i][0]
@@ -2020,9 +2020,9 @@ class MaskRCNN():
             nms_threshold=config.RPN_NMS_THRESHOLD,
             name="ROI",
             config=config)([rpn_class, rpn_bbox, anchors])
-        print(rpn_rois)
-        rpn_rois=custom_filtering(input_image,rpn_rois)
-        print(rpn_rois)
+        # print(rpn_rois)
+        # rpn_rois=custom_filtering(input_image,rpn_rois)
+        # print(rpn_rois)
         # custom_log(rpn_rois)
         if mode == "training":
             # Class ID mask to mark class IDs supported by the dataset the image
